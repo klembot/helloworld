4277c31ab0184efd5936bb07fd1f040cdccbb949
Chris Klimas <chris@twinery.org>
2022-07-19 00:10:55 -0400

    Use geolocation

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,12 @@ import {CurrentConditions} from './components/current-conditions';
 import {ForecastPeriod} from './components/forecast-period';
 import {useCurrentConditions} from './hooks/use-current-conditions';
 import {useForecast} from './hooks/use-forecast';
+import {useGeolocation} from './hooks/use-geolocation';
 import {formatDistance} from 'date-fns';
-
-const latitude = 39.28390440000004;
-const longitude = -76.74881219999997;
+import './app.css';
 
 export const App = () => {
+	const {error: locationError, latitude, longitude} = useGeolocation();
 	const {isLoading: forecastLoading, data: forecastData} = useForecast(
 		latitude,
 		longitude
@@ -15,7 +15,11 @@ export const App = () => {
 	const {isLoading: conditionsLoading, data: conditionsData} =
 		useCurrentConditions(latitude, longitude);
 
-	if (conditionsLoading || forecastLoading) {
+	if (locationError) {
+		return <p>Could not get location</p>;
+	}
+
+	if (!latitude || !longitude || conditionsLoading || forecastLoading) {
 		return <p>Loading...</p>;
 	}
 
